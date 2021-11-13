56fca752dc1fcbfdbab0269711e82c46606a87dd
Jory Tindall <jory@hypecreativestudios.com>
2021-11-13 12:35:22 -0800

    Build and version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,3 +2,4 @@ export { styled, getCssText, globalCss, css, theme, createTheme, keyframes, conf
 export { Button } from './src/components/button';
 export { Mega, H1, H2, H3, H4, H5, H6, Paragraph } from './src/components/typography';
 export { Icon, SocialIcon, Arrow } from './src/components/icons';
+export { Flex, Grid } from './src/components/layout';
--- dist/index.js
+++ dist/index.js
@@ -615,11 +615,145 @@ function Arrow(_a) {
             default: return null;
         }
     }
-    return (React__default["default"].createElement(IconWrapper, { xmlns: 'http://www.w3.org/2000/svg', fill: 'none', size: size, color: color, direction: direction }, iconRender(icon)));
+    return (React__default["default"].createElement(IconWrapper, { xmlns: 'http://www.w3.org/2000/svg', fill: 'none', size: size, color: color, direction: direction, css: {
+            '> *': {
+                fill: "" + color,
+            }
+        } }, iconRender(icon)));
 }
+
+var Flex = styled('div', {
+    boxSizing: 'border-box',
+    display: 'flex',
+    variants: {
+        direction: {
+            row: { flexDirection: 'row', },
+            column: { flexDirection: 'column', },
+            rowReverse: { flexDirection: 'row-reverse', },
+            columnRevers: { flexDirection: 'column-reverse', },
+        },
+        align: {
+            start: { alignItems: 'flex-start', },
+            center: { alignItems: 'center', },
+            end: { alignItems: 'flex-end', },
+            stretch: { alignItems: 'stretch', },
+            baseline: { alignItems: 'baseline', },
+        },
+        justify: {
+            start: { justifyContent: 'flex-start', },
+            center: { justifyContent: 'center', },
+            end: { justifyContent: 'flex-end', },
+            between: { justifyContent: 'space-between', },
+            around: { justifyContent: 'space-around', },
+        },
+        wrap: {
+            noWrap: { flexWrap: 'nowrap', },
+            wrap: { flexWrap: 'wrap', },
+            wrapReverse: { flexWrap: 'wrap-reverse', },
+        },
+        gap: {
+            1: { gap: '$1', },
+            2: { gap: '$2', },
+            3: { gap: '$3', },
+            4: { gap: '$4', },
+            5: { gap: '$5', },
+            6: { gap: '$6', },
+            7: { gap: '$7', },
+            8: { gap: '$8', },
+            9: { gap: '$9', },
+            10: { gap: '$10', },
+        }
+    },
+    defaultVariants: {
+        direction: 'row',
+        align: 'stretch',
+        justify: 'start',
+        wrap: 'noWrap',
+    }
+});
+
+var Grid = styled('div', {
+    boxSizing: 'border-box',
+    display: 'grid',
+    variants: {
+        align: {
+            start: { alignItems: 'start', },
+            center: { alignItems: 'center', },
+            end: { alignItems: 'end', },
+            stretch: { alignItems: 'stretch', },
+            baseline: { alignItems: 'baseline', },
+        },
+        justify: {
+            start: { justifyContent: 'start', },
+            center: { justifyContent: 'center', },
+            end: { justifyContent: 'end', },
+            between: { justifyContent: 'space-between', },
+            around: { justifyContent: 'space-around', },
+        },
+        flow: {
+            row: { gridAutoFlow: 'row', },
+            column: { gridAutoFlow: 'column', },
+            dense: { gridAutoFlow: 'dense', },
+            rowDense: { gridAutoFlow: 'row dense', },
+            columnDense: { gridAutoFlow: 'column dense', },
+        },
+        columns: {
+            1: { gridTemplateColumns: 'repeat(1, 1fr)', },
+            2: { gridTemplateColumns: 'repeat(2, 1fr)', },
+            3: { gridTemplateColumns: 'repeat(3, 1fr)', },
+            4: { gridTemplateColumns: 'repeat(4, 1fr)', },
+            5: { gridTemplateColumns: 'repeat(5, 1fr)', },
+            6: { gridTemplateColumns: 'repeat(6, 1fr)', },
+            7: { gridTemplateColumns: 'repeat(7, 1fr)', },
+            8: { gridTemplateColumns: 'repeat(8, 1fr)', },
+            9: { gridTemplateColumns: 'repeat(9, 1fr)', },
+            10: { gridTemplateColumns: 'repeat(10, 1fr)', },
+            11: { gridTemplateColumns: 'repeat(11, 1fr)', },
+            12: { gridTemplateColumns: 'repeat(12, 1fr)', },
+        },
+        gap: {
+            1: { gap: '$1', },
+            2: { gap: '$2', },
+            3: { gap: '$3', },
+            4: { gap: '$4', },
+            5: { gap: '$5', },
+            6: { gap: '$6', },
+            7: { gap: '$7', },
+            8: { gap: '$8', },
+            9: { gap: '$9', },
+            10: { gap: '$10', },
+        },
+        gapX: {
+            1: { columnGap: '$1', },
+            2: { columnGap: '$2', },
+            3: { columnGap: '$3', },
+            4: { columnGap: '$4', },
+            5: { columnGap: '$5', },
+            6: { columnGap: '$6', },
+            7: { columnGap: '$7', },
+            8: { columnGap: '$8', },
+            9: { columnGap: '$9', },
+            10: { columnGap: '$10', },
+        },
+        gapY: {
+            1: { rowGap: '$1', },
+            2: { rowGap: '$2', },
+            3: { rowGap: '$3', },
+            4: { rowGap: '$4', },
+            5: { rowGap: '$5', },
+            6: { rowGap: '$6', },
+            7: { rowGap: '$7', },
+            8: { rowGap: '$8', },
+            9: { rowGap: '$9', },
+            10: { rowGap: '$10', },
+        },
+    }
+});
 
 exports.Arrow = Arrow;
 exports.Button = Button;
+exports.Flex = Flex;
+exports.Grid = Grid;
 exports.H1 = H1;
 exports.H2 = H2;
 exports.H3 = H3;
