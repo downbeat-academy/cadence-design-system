43318b50e3582b1384a1d9171a7d98227bf7bbc0
Jory Tindall <jory@hypecreativestudios.com>
2021-12-03 15:56:56 -0800

    Added brand components, logos

---
+++ src/components/badge/Badge.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { styled } from '../../stitches.config'
+import { Icon } from '../icons'
+
+const Wrapper = styled('span', {
+    fontFamily: '$interfaceBody',
+    fontSize: '$interfaceExtraSmall',
+    lineHeight: '$interfaceBody',
+    display: 'flex',
+    flexDirection: 'row',
+    flexGrow: '0',
+    flexShrink: '0',
+    flexWrap: 'no-wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '$2',
+    padding: '$1 $3',
+    borderRadius: '1rem',
+
+    variants: {
+        type: {
+            neutral: {
+                background: '$accent100',
+                color: '$accent800',
+            },
+            info: {
+                background: '$blueberry000',
+                color: '$blackberry600'
+            },
+            success: {
+                background: '$kale100',
+                color: '$kale800',
+            },
+            warning: {
+                background: '$pineapple200',
+                color: '$pineapple800',
+            },
+            error: {
+                background: '$pomegranate100',
+                color: '$pomegranate700',
+            }
+        }
+    },
+
+    defaultVariants: {
+        type: 'neutral',
+    }
+})
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
 import type { NextPage } from 'next';
+import { Badge } from '../components/badge'
+import { Icon } from '../components/icons'
+import { Logo } from '../components/brand'
 
 const Home: NextPage = () => {
 	return (
 		<div>
 			<p>Home Page</p>
+			{/* <Badge type='error'>
+				<Icon icon='error' size='extraSmall' />Badge text
+			</Badge> */}
+			<Logo
+				type='lockup'
+				color='$colors$passionFruit500'
+				width='500'
+			/>
 		</div>
 	);
 };
