9075f7066114264c534b0f0244aed76e5f7cf8d3
Jory Tindall <jory@hypecreativestudios.com>
2022-01-18 21:14:32 -0800

    Cleanup of responsive typography

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
 import type { NextPage } from 'next';
-import { H1, H2, H3, H4, H5, H6 } from '../components/typography'
+import { Divider } from '../components/divider'
+import { Mega, H1, H2, H3, H4, H5, H6 } from '../components/typography'
 
 const Home: NextPage = () => {
 	return (
 		<div>
+			<Mega>Mega headline</Mega>
 			<H1 context='display'>Heading 1</H1>
 			<H2 context='display'>Heading 2</H2>
 			<H3 context='display'>Heading 3</H3>
 			<H4 context='display'>Heading 4</H4>
 			<H5 context='display'>Heading 5</H5>
 			<H6 context='display'>Heading 6</H6>
+			<Divider />
+			<H1 context='interface'>Heading 1</H1>
+			<H2 context='interface'>Heading 2</H2>
+			<H3 context='interface'>Heading 3</H3>
+			<H4 context='interface'>Heading 4</H4>
+			<H5 context='interface'>Heading 5</H5>
+			<H6 context='interface'>Heading 6</H6>
 		</div>
 	);
 };
