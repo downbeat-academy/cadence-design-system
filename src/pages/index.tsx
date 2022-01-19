e0bb5736e00661c0fc955db724109cbdb3dfb7d2
Jory Tindall <jory@hypecreativestudios.com>
2022-01-18 21:07:09 -0800

    Updated typography to be fluid

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from 'next';
-import { Divider } from '../components/divider'
+import { H1, H2, H3, H4, H5, H6 } from '../components/typography'
 
 const Home: NextPage = () => {
 	return (
 		<div>
-			<p>Home Page</p>
-			<Divider />
+			<H1 context='display'>Heading 1</H1>
+			<H2 context='display'>Heading 2</H2>
+			<H3 context='display'>Heading 3</H3>
+			<H4 context='display'>Heading 4</H4>
+			<H5 context='display'>Heading 5</H5>
+			<H6 context='display'>Heading 6</H6>
 		</div>
 	);
 };
