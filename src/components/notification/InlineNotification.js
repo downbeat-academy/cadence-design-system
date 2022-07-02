c8144efce6d7cbda29ec3aa8b12ff68e207e76c5
Jory Tindall <jorytindall@gmail.com>
2022-07-02 11:28:24 -0700

    Adding notification components

--- src/components/notification/InlineNotification.js
+++ src/components/notification/InlineNotification.js
@@ -1,4 +1,4 @@
-import { styled } from '@/stitches.config'
+import { styled } from '../../stitches.config'
 import { Paragraph } from '@/components/typography'
 import { Flex } from '@/components/layout'
 
